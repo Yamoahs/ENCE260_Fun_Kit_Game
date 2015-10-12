0b427898a70dea85cccbec4aa7e27eb77e8b915f
Samuel <sya57@uclive.ac.nz>
2015-10-13 05:28:08 +1300

    Removed redundant functions

--- GAME3.c
+++ GAME3.c
@@ -11,22 +11,6 @@
 #define MAX_TICKS 150
 #define FACTOR 10
 
-void ledmat (void)
-{
-  pio_config_set(LEDMAT_COL1_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_COL2_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_COL3_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_COL4_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_COL5_PIO, PIO_OUTPUT_HIGH);
-
-  pio_config_set(LEDMAT_ROW1_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_ROW2_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_ROW3_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_ROW4_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_ROW5_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_ROW6_PIO, PIO_OUTPUT_HIGH);
-  pio_config_set(LEDMAT_ROW7_PIO, PIO_OUTPUT_HIGH);
-}
 
 void nav_move (uint8_t* current_column)
 {
@@ -120,7 +104,6 @@ int main (void)
     system_init ();
     display_init ();
     navswitch_init ();
-    ledmat();
     pacer_init (600);
     uint8_t current_column = START_COL;
     int8_t firing_row = START_ROW - 1;
