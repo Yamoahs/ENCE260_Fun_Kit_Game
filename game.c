110d25a80d6b1722ed158a444bb1b7a3e74da6a3
Ariel <aya37@uclive.ac.nz>
2015-10-14 02:28:23 +1300

    ariels code

---
+++ game.c
@@ -0,0 +1,399 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "system.h"
+#include "tinygl.h"
+#include "pacer.h"
+#include "ir_serial.h"
+#include "ir_uart.h"
+#include "flasher.h"
+#include "objects.h"
+#include "eeprom.h"
+#include "uint8toa.h"
+#include "font3x5_1.h"
+#include "navswitch.h"
+#include "led.h"
+#include "timer.h"
+
+
+enum {LOOP_RATE = 500};
+enum {FLASHER_UPDATE_RATE = LOOP_RATE};
+enum {BUTTON_POLL_RATE = 100};
+enum {GAME_UPDATE_RATE = 500};
+enum {IR_UPDATE_RATE = 1000};
+enum {GAME_OVER_PERIOD = 2};
+enum {BUTTON_HOLD_PERIOD = 1};
+
+#define MESSAGE_RATE 30
+
+/** Define flasher modes.  */
+static flasher_pattern_t flasher_patterns[] =
+{
+    /** POLL_RATE, MOD_FREQ, MOD_DUTY, FLASHER_PERIOD,
+       FLASHER_DUTY, FLASHES, PERIOD.  */
+    {FLASHER_PATTERN (FLASHER_UPDATE_RATE, 100, 100, 0.4, 100, 1, 0.4)},
+    {FLASHER_PATTERN (FLASHER_UPDATE_RATE, 100, 100, 0.4, 100, 1, 0.4)},
+    {FLASHER_PATTERN (FLASHER_UPDATE_RATE, 200, 100, 0.1, 50, 1, 0.1)},
+};
+
+typedef enum {FLASH_MODE_PLAYER, FLASH_MODE_BOMB,
+			  FLASH_MODE_NUM} flash_mode_t;
+
+
+typedef enum {STATE_DECIDE, STATE_WAIT, STATE_INIT, STATE_INVITE, STATE_START,
+              STATE_PLAYING, STATE_OVER, 
+              STATE_READY} state_t;
+              
+typedef struct
+{
+    uint8_t games;
+} game_data_t;
+
+            
+/** Draw pixel on display.  */
+static void
+display_handler (void *data, uint8_t col, uint8_t row, pix_t type)
+{
+    uint8_t *display = data;
+    uint8_t pixel;
+
+    pixel = row * TINYGL_WIDTH + col;
+    display[pixel] = type;
+}
+
+static void
+game_start (game_data_t *data, uint8_t ME)
+{
+    tinygl_clear ();
+    data->games++;
+    tossing_start(ME);
+    eeprom_write (0, data, sizeof (*data));
+}
+
+
+
+
+int main (void)
+{
+	uint8_t navswitch_ticks;
+    uint8_t game_ticks;
+    uint8_t ir_ticks;
+    uint8_t game_over_ticks;
+    uint8_t navswitch_down_count;
+    state_t state = STATE_INIT;
+    flasher_t flashers[PIX_TYPE_NUM];
+    uint8_t flasher_state[PIX_TYPE_NUM];
+    flasher_obj_t flashers_info[PIX_TYPE_NUM];
+    uint8_t display[TINYGL_WIDTH * TINYGL_HEIGHT];
+    uint8_t i;
+    uint8_t j;
+    uint8_t ME = 0;
+    uint8_t OPPONENT = 0;
+    uint8_t RECEIVED = 0;
+    uint8_t GAME_INVITE = 0;
+    uint8_t GAME_ACCEPT = 0;
+    uint8_t DUMMY = -2;
+    game_data_t data;
+    
+	
+    system_init ();
+    
+    
+    eeprom_read (0, &data, sizeof (data));
+    
+    for (i = 0; i < ARRAY_SIZE (flashers); i++)
+    {
+        flashers[i] = flasher_init (&flashers_info[i]);
+        flasher_state[i] = 0;
+    }
+
+    for (i = 0; i < ARRAY_SIZE (display); i++)
+        display[i] = 0;
+
+    /* Set up flash patterns for different pixel types.  */
+    flasher_pattern_set (flashers[PIX_PLAYER],
+                         &flasher_patterns[FLASH_MODE_PLAYER]);
+    flasher_pattern_set (flashers[PIX_BOMB],
+                         &flasher_patterns[FLASH_MODE_BOMB]);
+                         
+    tinygl_init (LOOP_RATE);
+    tinygl_font_set (&font3x5_1);
+    tinygl_text_speed_set (MESSAGE_RATE);
+	tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+	tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
+    
+    navswitch_init();
+    ir_serial_init();
+
+    tossing_init (GAME_UPDATE_RATE, TINYGL_WIDTH, TINYGL_HEIGHT, 
+                 display_handler, display);
+
+    navswitch_ticks = 0;
+    game_ticks = 0;
+    ir_ticks = 0;
+    game_over_ticks = 0;
+    navswitch_down_count = 0;
+
+    pacer_init (LOOP_RATE);
+    
+
+    while (1)
+    {
+		pacer_wait ();
+		
+		if (state == STATE_PLAYING)
+        {
+            uint8_t *src;
+
+            /* Update flasher states.  NB, the first flasher is always off.  */
+            for (i = 1; i < ARRAY_SIZE (flashers); i++)
+                flasher_state[i] = flasher_update (flashers[i]);
+            
+            /* Update display.  */
+            src = display;
+            for (j = 0; j < TINYGL_HEIGHT; j++)
+                for (i = 0; i < TINYGL_WIDTH; i++)
+                {
+                    tinygl_point_t point = {i, j};
+
+                    tinygl_draw_point (point, flasher_state[*src++]);
+                }
+        }
+        
+        /* Advance messages and refresh display.  */
+        tinygl_update ();
+        
+        game_ticks++;
+        if (game_ticks >= LOOP_RATE / GAME_UPDATE_RATE)
+        {
+            game_ticks = 0;
+                
+            switch (state)
+            {
+            case STATE_PLAYING:
+				if (! tossing_update (DUMMY))
+				{
+					tinygl_clear ();
+					if (DUMMY == 5) {
+						OPPONENT = 0;
+					} else {
+						ME = 0;
+					}
+                    game_over_ticks = 0;                    
+                    led_set (LED1, 0);
+                    state = STATE_OVER;
+				}
+				DUMMY = -2;
+				break;
+                
+                
+            case STATE_INIT:
+                tinygl_text ("HOT POTATO");
+                state = STATE_DECIDE;
+                break;
+                
+            case STATE_OVER:
+                if (ME == 0) {
+					tinygl_text ("LOSER!");
+					} else if (OPPONENT == 0) {
+						tinygl_text ("WINNER!");
+					}
+                    state = STATE_DECIDE;
+                /* Fall through.  */
+
+            default:
+                break;
+                
+            case STATE_START:
+                /* Turn that bloody blimey space invader off...  */
+                game_start (&data, ME);
+                state = STATE_PLAYING;
+                break;
+            }
+        }
+        
+        
+        /* Poll navswitch.  */
+        navswitch_ticks++;
+        if (navswitch_ticks >= LOOP_RATE / BUTTON_POLL_RATE)
+        {
+            navswitch_ticks = 0;
+
+            navswitch_update ();
+
+            if (navswitch_down_p (NAVSWITCH_EAST))
+                navswitch_down_count++;
+            else
+                navswitch_down_count = 0;
+
+            if (navswitch_down_count >= BUTTON_POLL_RATE * BUTTON_HOLD_PERIOD)
+                state = STATE_INIT;
+
+			if (navswitch_push_event_p (NAVSWITCH_NORTH))
+            {
+                switch (state)
+                {
+                case STATE_DECIDE:
+					ME = 1;
+					OPPONENT = 2;
+					tinygl_clear ();
+					tinygl_text("P1");
+					break;
+                default:
+                    break;
+                }
+            }
+            
+            if (navswitch_push_event_p (NAVSWITCH_SOUTH))
+            {
+                switch (state)
+                {
+                case STATE_DECIDE:
+					ME = 2;
+					OPPONENT = 1;
+					tinygl_clear ();
+					tinygl_text("P2");
+					break;
+                default:
+                    break;
+                }
+            }
+            
+            if (navswitch_push_event_p (NAVSWITCH_EAST))
+            {
+                switch (state)
+                {
+                case STATE_WAIT:
+					break;
+
+                case STATE_PLAYING:
+                    player_move_right ();
+                    break;
+
+                default:
+                    break;
+                }
+            }
+
+            if (navswitch_push_event_p (NAVSWITCH_WEST))
+            {
+                switch (state)
+                {
+                case STATE_PLAYING:
+                    player_move_left ();
+                    break;
+				default:
+                    break;
+                }
+            }
+
+
+            if (navswitch_push_event_p (NAVSWITCH_PUSH))
+            {
+                switch (state)
+                {
+				case STATE_READY:
+					tinygl_clear ();
+					GAME_INVITE = 7;
+                    state = STATE_INVITE;
+                    break;
+                case STATE_DECIDE:
+					if (ME == 0) {
+						break;
+					}
+					else {
+						if (ME == 1) {
+							tinygl_clear ();
+							tinygl_text("YOU ARE P1");
+						}
+						else if (ME == 2) {
+							tinygl_clear ();
+							tinygl_text("YOU ARE P2");
+						}
+						state = STATE_WAIT;
+					}
+					break;
+						
+                case STATE_PLAYING:
+                    player_fire ();
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        
+        ir_ticks++;
+        if (ir_ticks >= LOOP_RATE / IR_UPDATE_RATE)
+        {
+            ir_ticks = 0;
+                
+            switch (state)
+            {
+            case STATE_PLAYING:
+				if (ir_serial_receive (&DUMMY) == IR_SERIAL_OK) {
+					}
+					break;
+          
+            case STATE_WAIT:
+				if (ir_serial_receive (&RECEIVED) == IR_SERIAL_OK) {
+					if (RECEIVED == 3) {
+					state = STATE_READY;
+				}
+			}
+			DUMMY = 6;
+					ir_serial_transmit(OPPONENT);
+				break;
+			
+			case STATE_DECIDE:
+				if (ir_serial_receive (&ME) == IR_SERIAL_OK) {
+					if (ME == 1) {
+						OPPONENT = 2;
+						ir_serial_transmit(3);
+						tinygl_clear ();
+						tinygl_text("YOU ARE P1");
+						state = STATE_READY;
+					}
+					if (ME == 2) {
+						OPPONENT = 1;
+						ir_serial_transmit(3);
+						tinygl_clear ();
+						tinygl_text("YOU ARE P2");
+						state= STATE_READY;
+						
+				}
+			}
+				break;
+			case STATE_INVITE:
+				if (ir_serial_receive (&GAME_ACCEPT) == IR_SERIAL_OK) {
+					if (GAME_ACCEPT == 5) {	
+					state = STATE_START;
+					}
+				}
+				ir_serial_transmit(GAME_INVITE);
+				break;
+				
+				
+			case STATE_READY:
+				if (ir_serial_receive (&GAME_INVITE) == IR_SERIAL_OK) {
+					if (GAME_INVITE == 7) {
+						tinygl_clear ();
+					GAME_ACCEPT = 5;
+					ir_serial_transmit(GAME_ACCEPT);	
+					state = STATE_START;
+				}
+			}
+				break;
+            default:
+                break;
+                            
+            case STATE_OVER:
+				ir_serial_transmit(5);
+			}
+		
+        }
+        
+        
+        
+    }
+}
---
+++ objects.c
@@ -0,0 +1,332 @@
+#include <stdlib.h>
+#include "objects.h"
+#include "ir_serial.h"
+#include "led.h"
+
+#ifndef DISPLAY_INTERLACE
+#define DISPLAY_INTERLACE 0
+#endif   
+
+#if DISPLAY_INTERLACE
+/* With an interlaced display we do not h#include "ir_serial.h"ave odd numbered rows and
+   columns.  */
+enum {BOMB_INC = 2, PLAYER_INC = 2};
+#else
+enum {BOMB_INC = 1, PLAYER_INC = 1};
+#endif
+
+enum {BOMB_SPEED_DEFAULT = 3};
+enum {BOMB_NUM_DEFAULT = 3};
+enum {CHANCE_SPEED_DEFAULT = 2};
+
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
+static tossing_t data;
+static tossing_t * const tossing = &data;
+
+
+
+void 
+bomb_speed_set (uint8_t bomb_speed)
+{
+    tossing->bomb.move_period = tossing->poll_rate / bomb_speed;
+}
+
+void 
+blow_up_speed_set (uint8_t blow_up_speed)
+{
+    tossing->blow_up_period = tossing->poll_rate / blow_up_speed;
+}
+
+void
+bomb_num_set (uint8_t bomb_num)
+{
+    tossing->bomb.num = min(bomb_num, BOMB_MAX);
+}
+
+
+
+void
+tossing_init (uint16_t poll_rate,
+             uint8_t x_size, uint8_t y_size,
+             void (*display_handler) (void *data, uint8_t row, uint8_t col,
+                                      pix_t type),
+             void *display_data)
+{ 
+    tossing->size.x = x_size;
+    tossing->size.y = y_size;
+    tossing->display_hook = display_handler;
+    tossing->display_data = display_data;
+    tossing->active = 0;
+    tossing->poll_rate = poll_rate;
+
+    
+    bomb_num_set (BOMB_NUM_DEFAULT);
+    bomb_speed_set (BOMB_SPEED_DEFAULT);
+    blow_up_speed_set (CHANCE_SPEED_DEFAULT);
+}
+
+static void
+pixel_set (pos_t *pos, pix_t type)
+{
+    tossing->display_hook (tossing->display_data, pos->x, pos->y, type);
+}
+
+static void player_move (int8_t inc)
+{
+    int8_t player_x;
+
+    pixel_set (&tossing->player, PIX_OFF);
+
+    player_x = tossing->player.x + inc;
+    if (player_x >= tossing->size.x)
+        player_x = 0;
+    else if (player_x < 0)
+        player_x = tossing->size.x - 1;
+    tossing->player.x = player_x;
+
+   pixel_set (&tossing->player, PIX_PLAYER);
+}
+
+/* Return true if move off display.  */
+static bool bomb_move_up (obj_t *bomb)
+{
+    /* Erase the previous position of the shell except if still
+       at gun.  */
+    if (bomb->pos.x != tossing->player.x
+        || bomb->pos.y != tossing->player.y)
+        pixel_set (&bomb->pos, PIX_OFF);
+
+    /* Shells only go straight up.  */
+    bomb->pos.y -= BOMB_INC;
+    
+    if (bomb->pos.y < 0)
+        return 1;
+
+    pixel_set (&bomb->pos, PIX_BOMB);
+    return 0;
+}
+
+/* Return true if reaches player's row.  */
+static bool bomb_move_down (obj_t *bomb)
+{
+    /* Erase the previous position of the shell except if still
+       at gun.  */#include "ir_serial.h"
+    if (bomb->pos.x != tossing->player.x
+        || bomb->pos.y != tossing->player.y)
+        pixel_set (&bomb->pos, PIX_OFF);
+	
+	if (bomb->pos.y == tossing->player.y && bomb->pos.x == tossing->player.x) {
+		tossing->stats.bomb_caught = 1;
+		tossing->player.turn = 1;
+		led_set (LED1, 1);
+	} else if (bomb->pos.y == tossing->player.y && bomb->pos.x != tossing->player.x) {
+		tossing->active = 0;
+	}
+    /* Shells go straight down.  */
+    bomb->pos.y += BOMB_INC;
+    
+    if (bomb->pos.y > 7)
+        return 1;
+
+    pixel_set (&bomb->pos, PIX_BOMB);
+    
+    return 0;
+}
+
+
+static void
+bomb_create (void)
+{
+    int8_t i;
+
+    for (i = 0; i < tossing->bomb.num; i++)
+    {
+        obj_t *bomb = &tossing->bomb.array[i];
+
+        if (bomb->active)
+            continue;
+            
+        bomb->pos = tossing->player;
+        bomb->active = 1;
+        bomb->direction = 1;
+        /* Don't turn shell on initially since it will erase the gun.  */
+        return;
+    }
+    /* If we get to here then we already have too many shells.  */
+}
+
+static void
+bomb_create_down (int8_t pos_x)
+{
+    int8_t i;
+
+    for (i = 0; i < tossing->bomb.num; i++)
+    {
+        obj_t *bomb = &tossing->bomb.array[i];
+
+        if (bomb->active)
+            continue;
+            
+        bomb->pos.x = pos_x;
+        bomb->pos.y = -1;
+        bomb->active = 1;
+        bomb->direction = 0;
+        /* Don't turn shell on initially since it will erase the gun.  */
+        return;
+    }
+    /* If we get to here then we already have too many shells.  */
+}
+
+static void
+bomb_kill (obj_t *bomb)
+{
+    bomb->active = 0;
+}
+
+
+static void
+bombs_move ()
+{
+    tossing->bomb.move_clock++;
+    if (tossing->bomb.move_clock < tossing->bomb.move_period)
+        return;
+    tossing->bomb.move_clock = 0;
+
+    /* Shells move until they hit an alien or move off the display.  */
+
+        obj_t *bomb = &tossing->bomb.array[0];
+
+        if (bomb->active) {
+		if (bomb->direction == 1) {
+			if (bomb_move_up (bomb)) {
+				ir_serial_transmit(bomb->pos.x);
+				bomb_kill (bomb);
+			}
+		}
+		if (bomb->direction == 0) {
+			if (bomb_move_down (bomb)) {
+				bomb_kill (bomb);
+			}
+     }
+    
+}
+}
+
+/* Update the state of the game.  */
+bool tossing_update (int8_t DUMMY)
+{
+	int8_t pos_x = -1;
+    if (DUMMY >= 0 && DUMMY <= 4) {
+		
+		pos_x = 4-DUMMY;
+		bomb_create_down(pos_x);	
+	}
+	if (DUMMY == 5) {
+		tossing->active = 0;
+	}
+	
+    /* Allow playing with the gun even if game inactive.  */
+    bombs_move ();
+    if (blow_up_chance() && tossing->player.turn == 1) {
+		tossing->active = 0;
+	}
+    
+    if (!tossing->active)
+        return 0;
+    return 1;
+}
+
+/* Move the gun position to the right wrapping back around on left.  */
+void
+player_move_right (void)
+{
+    player_move (PLAYER_INC);
+}
+
+
+/* Move the gun position to the left wrapping back around on right.  */
+void
+player_move_left (void)
+{
+    player_move (-PLAYER_INC);
+}
+
+
+/* Fire the gun.  */
+void
+player_fire (void)
+{
+	if (tossing->player.turn == 1) {
+    bomb_create ();
+    if (tossing->stats.bomb_tossed <= 10) {
+		tossing->stats.bomb_tossed++;
+	}
+    bomb_speed_set (BOMB_SPEED_DEFAULT + tossing->stats.bomb_tossed);
+	led_set (LED1, 0);
+    tossing->player.turn = 0;
+}
+}
+
+/* Start a new game.  */
+void
+tossing_start (uint8_t ME)
+{
+    int8_t i;
+    int8_t j;
+
+    tossing->bomb.move_clock = 0;
+    
+    for (i = 0; i < tossing->bomb.num; i++)
+    {
+        obj_t *bomb = &tossing->bomb.array[i];
+
+        bomb->active = 0;
+    }
+
+    /* Turn all pixels off.  */
+    for (i = 0; i < tossing->size.x; i++)
+        for (j = 0; j < tossing->size.y; j++)
+        {
+            pos_t pos = {i, j, 0};
+
+            pixel_set (&pos, PIX_OFF);
+        }
+
+    /* Display gun in centre of display.  */
+    tossing->player.x = (tossing->size.x / 2) & ~1;
+    tossing->player.y = tossing->size.y - 1;
+    if (ME == 1) {
+		tossing->player.turn = 1;
+		led_set (LED1, 1);
+	} else if (ME == 2) {
+		tossing->player.turn = 0;
+	}
+    pixel_set (&tossing->player, PIX_PLAYER);
+
+    tossing->stats.bomb_tossed = 0;
+    tossing->active = 1;
+}
+
+
+
+bool blow_up_chance(void) 
+{	
+	tossing->blow_up_clock++;
+    if (tossing->blow_up_clock < tossing->blow_up_period)
+        return 0;
+    tossing->blow_up_clock = 0;
+    
+    
+	uint8_t chance;	
+	chance = rand () % 200 + 1;
+	if (chance == 1) {
+		return 1;
+	}
+	return 0;
+}
+	
+	
+	
+
+
